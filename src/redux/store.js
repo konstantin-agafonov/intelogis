1730cbe79a4f182f4ee75115417b5e4fbe69f026
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2023-02-04 18:56:28 +0300

    Install redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import shippingMapReducer from './reducers/shippingMap.reducer';
 import shippingRequestsTableReducer from './reducers/shippingRequestsTable.reducer';
+import {logger} from "redux-logger";
 
 export const store = configureStore({
     reducer: {
         shippingMap: shippingMapReducer,
         shippingRequestsTable: shippingRequestsTableReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger),
 })
